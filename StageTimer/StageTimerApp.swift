2815d83dd6e27f9dbf517058f6fd205aa270b253
Julian Beck <ju-fa-beck@t-online.de>
2025-03-23 17:07:05 +0100

    Initial Commit

---
+++ StageTimer/StageTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StageTimerApp.swift
+//  StageTimer
+//
+//  Created by Julian Beck on 23.03.25.
+//
+
+import SwiftUI
+
+@main
+struct StageTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
